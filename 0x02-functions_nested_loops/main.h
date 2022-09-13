e2ad6713584f9abe0c8a6f052911e6ecb18c85e9
ZeehnohCodes <markgiftt@gmail.com>
2022-09-13 14:47:37 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#1fndef HOLBERTON_H_INCLUDED
-#define HOLBERTON_H_INCLUDED
+#ifndef main_h_
+#define main_h_
 
 int _putchar(char);
 void print_alphabet(void);
@@ -13,8 +13,7 @@ void jack_bauer(void);
 void times_table(void);
 int add(int a, int b);
 void print_to_98 (int n);
-vold print_times_table(int n);
+void print_times_table(int n);
 void num(int r, int n);
 
-#endif
-
+#endif 
