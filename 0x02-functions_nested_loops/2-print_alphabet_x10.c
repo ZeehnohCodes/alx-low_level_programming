b0f8d63295975dbc8d74e379a16565f3f2c68f6b
ZeehnohCodes <markgiftt@gmail.com>
2022-09-13 16:19:14 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,12 +8,12 @@ void print_alphabet_x10(void)
 {
 	char c, i;
 
-	for (i = o; i <= 9; i++)
+	for (i = 'o'; i <= 9; i++)
 	{
 		for (c = 'a'; c <= 'z'; c++)
 		{
 			_putchar(c);
 		}
-		_putchar(*\n*)
+		_putchar('\n')
 	}
 }
