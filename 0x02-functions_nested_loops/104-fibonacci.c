32a3ba197acd3474d446ef2bed372027c6b47d1f
ZeehnohCodes <markgiftt@gmail.com>
2022-09-14 18:25:13 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -15,7 +15,6 @@ int count;
 printf("%lu, %lu, ", bk1, bk2);
 for (count = 2; count < 98; count++)
 {
-if (bk1 + bk2 > LARGEST || fr2 > 0 || fr1 > 0)	
 {
 hold1 = (bk1 + bk2) / LARGEST;
 hold2 = (bk1 + bk2) % LARGEST;
