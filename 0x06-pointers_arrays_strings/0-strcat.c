eb9c5f7968849a7ef04d39492c96cfa5649733ba
ZeehnohCodes <markgiftt@gmail.com>
2022-09-21 13:09:31 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,23 +9,23 @@
 
 char *_strcat(char *dest, char *src)
 {
-    int i;
-    int j;
+int i;
+int j;
 
-    i = 0;
-    j = 0;
+i = 0;
+j = 0;
 
-    while (dest[i] != '\0')
-    {
-        i++;
-    }
-    while (src[j] != '\0')
-    {
-        dest[i] = src[j];
-        j++;
-        i++;
-    }
+while (dest[i] != '\0')
+{
+i++;
+}
+while (src[j] != '\0')
+{
+dest[i] = src[j];
+j++;
+i++;
+}
 
-    dest[i] = '\0';
-    return (dest);
+dest[i] = '\0';
+return (dest);
 }
