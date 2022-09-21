5b0149496c510b65d30de83cc20f5e5878bf4b32
ZeehnohCodes <markgiftt@gmail.com>
2022-09-21 13:22:05 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,15 +9,15 @@
 
 int _strcmp(char *s1, char *s2)
 {
-    int i = 0, op = 0;
+int i = 0, op = 0;
 
-    while (op == 0)
-    {
-        if (((s1 + i) == '\0') && ((s2 + i) == '\0'))
-            break;
-        op = *(s1 + i) - *(s2 + i);
-        i++;
-    }
+while (op == 0)
+{
+if (((s1 + i) == '\0') && ((s2 + i) == '\0'))
+break;
+op = *(s1 + i) - *(s2 + i);
+i++;
+}
 
-    return (op);
+return (op);
 }
