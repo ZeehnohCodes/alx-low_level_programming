c79a190fe698ffe0c18de8e4e9b21798e82db880
ZeehnohCodes <markgiftt@gmail.com>
2022-10-21 13:52:25 -0700

    all

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,9 +1,7 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 /**
  * struct list_s - singly linked list
@@ -12,7 +10,7 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for Holberton project
+ *              for project
  */
 typedef struct list_s
 {
@@ -26,6 +24,5 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-int _strlen(const char *str);
 
-#endif / LISTS_H /
+#endif /* LISTS_H */
