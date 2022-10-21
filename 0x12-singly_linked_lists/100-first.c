543df7f59c01eaca3b8a9c448649e5027b32cb22
ZeehnohCodes <markgiftt@gmail.com>
2022-10-21 14:21:06 -0700

    all

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,50 +1,13 @@
-#include "lists.h"
-#include <string.h>
+#include <stdio.h>
+
+void _attribute_((constructor)) hare(void);
 
 /**
- * add_node_end - Adds a new node at the end
- *                of a list_t list.
- * @head: A pointer the head of the list_t list.
- * @str: The string to be added to the list_t list.
- *
- * Return: If the function fails - NULL.
- *         Otherwise - the address of the new element.
+ * hare - Prints a string before the
+ *        main function is executed.
  */
-
-list_t *add_node_end(list_t **head, const char *str)
+void hare(void)
 {
-	char *dup;
-	int len;
-	list_t *new, *last;
-
-	new = malloc(sizeof(list_t));
-	if (new == NULL)
-		return (NULL);
-
-	dup = strdup(str);
-	if (str == NULL)
-	{
-		free(new);
-		return (NULL);
-	}
-
-	for (len = 0; str[len];)
-		len++;
-
-	new->str = dup;
-	new->len = len;
-	new->next = NULL;
-
-	if (*head == NULL)
-		*head = new;
-
-	else
-	{
-		last = *head;
-		while (last->next != NULL)
-			last = last->next;
-		last->next = new;
-	}
-
-	return (*head);
+	printf("You're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n");
 }
